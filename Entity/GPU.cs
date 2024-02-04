3e1b21d218bea3c2d517920908ef05cff8112dcd
Patrick Silva <pfivel@gmail.com>
2024-02-04 15:14:46 -0700

    Add project files.

---
+++ Entity/GPU.cs
@@ -0,0 +1,10 @@
+﻿namespace GPUTestAnalysisFeb4.Entity
+{
+    public class GPU
+    {
+        public string? Type { get; set; }
+        public string? Parameter { get; set; }
+        public string? Expression { get; set; }
+        public string? Destription { get; set; }
+    }
+}
